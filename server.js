1096ba980b43810580071545ed51ec9d1bafaa9f
DTPsykko <Psychotherapy420@gmail.com>
2022-05-25 11:54:52 -0700

    updating

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3000, ()=> {
+app.listen(process.env.PORT || 3003, ()=> {
 	console.log(`app is running on port 3003 ${process.env.PORT}`)
 })
