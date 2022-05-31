7101b59a48d5e1be2404e9636109242c434cc3d0
DTPsykko <Psychotherapy420@gmail.com>
2022-05-31 12:12:37 -0700

    yes

--- server.js
+++ server.js
@@ -41,5 +41,5 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(process.env.PORT || 3003, ()=> {
-	console.log(`app is running on port 3003 ${process.env.PORT}`)
+	console.log(`app is running on port ${process.env.PORT}`)
 })
